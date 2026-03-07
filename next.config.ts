feda6b40c2dbcf656f448a336581b022d5752b31
Gnomerock <nuttakorn.buajarern@gmail.com>
2026-03-07 21:51:47 +0700

    add deploy configuration

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
-    domains: ["avatars.githubusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
   },
 };
 
